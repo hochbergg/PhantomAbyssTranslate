b5afcc343f8828d7163907f5fbc074a6d2049aa7
Gal Hochberg <hochbergg@gmail.com>
2021-07-01 16:44:41 +0300

    Updated site with hovers and translations

--- generate_site.py
+++ generate_site.py
@@ -9,15 +9,18 @@ import jinja2
 import time
 import re
 import datetime
+import html
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = '12w1foShEkiWhKd1ktgwWi8zJv9icKjuQVwRCe17_SVg'
-RANGE_NAME = "'Glyph Handbook'!A1:X263"
+GLYPHS_RANGE_NAME = "'Suggested Meanings (Glyphs)'!A1:X264"
+HUB_TEXTS_RANGE_NAME = "'Writings (Hub)'!A1:P28"
+WALL_TEXTS_RANGE_NAME = "'Writings (Wall)'!A1:P68"
 
-def download_glyphs():
+def download_sheet_data(range):
 	"""Shows basic usage of the Sheets API.
 	Prints values from a sample spreadsheet.
 	"""
@@ -44,9 +47,12 @@ def download_glyphs():
 	# Call the Sheets API
 	sheet = service.spreadsheets()
 	result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-								range=RANGE_NAME).execute()
+								range=range).execute()
 	values = result.get('values', [])
+	return values;
 
+def download_glyphs(fname):
+	values = download_sheet_data(GLYPHS_RANGE_NAME)
 	glyphs = []
 	for r in values[1:]:
 		glyphs.append({
@@ -58,14 +64,36 @@ def download_glyphs():
 			"breakdown": r[12],
 		})
 	
-	with open('sheet_glyphs.json','w') as f:
+	with open(fname,'w') as f:
 		f.write(json.dumps({
 			"glyphs": glyphs,
 			"at": datetime.datetime.utcnow().timestamp()
 		}))
 
-def load_glyphs():
-	with open('sheet_glyphs.json','r') as f:
+def load_texts():
+	with open('texts.json','r') as f:
+		return json.load(f)
+		
+def download_texts(fname, range_str, text_data):
+	values = download_sheet_data(range_str)
+	texts = []
+	for i in range(1,len(values[1:])):
+		texts.append({
+			"number": i,
+			"text": text_data[str(i)],
+			"status": values[i][11],
+			"literal_translation": values[i][12],
+			"translation": values[i][13],
+		})
+	
+	with open(fname,'w') as f:
+		f.write(json.dumps({
+			"texts": texts,
+			"at": datetime.datetime.utcnow().timestamp()
+		}))
+
+def load_json_file(fname):
+	with open(fname,'r') as f:
 		return json.load(f)
 		
 		
@@ -77,24 +105,100 @@ def load_template(name):
 		
 
 def filter_replace_glyph_references(value):
-	return re.sub('#([0-9]{3})', '<a href="#glyph_\\1" class="glyph-link">#\\1</a>', value)
+	global glyphs
+	in_match = False;
+	out = []
+	
+	glyph_table = {g['number']:g['glyph'] for g in glyphs['glyphs']}
+	
+	for m in re.split('[#]([0-9]{3})', value):
+		if in_match:
+			out.append(f'<a href="glyphs.html#glyph_{m}" class="glyph-link text-white ancient-language" onmousemove="glyphMouseMove(event)" onmouseout="glyphMouseOut(event)" data-number="{m}">{html.escape(glyph_table[m])}</a>')
+			in_match = False
+		else:
+			out.append(m)
+			in_match = True
+			
+		
+	return ''.join(out)
+	
+	
+def filter_replace_text_references(value):
+	global glyphs
+	in_match = False
+	out = []
+	
+	#glyph_table = {g['number']:g['glyph'] for g in glyphs['glyphs']}
+	
+	for m in re.split('Wall ([0-9]{1,2})', value):
+		if in_match:
+			out.append(f'<a href="wall_texts.html#text_{m}" class="text-link text-white" onmousemove="wallMouseMove(event)" onmouseout="wallMouseOut(event)" data-number="{m}">Wall {m}</a>')
+			in_match = False
+		else:
+			out.append(m)
+			in_match = True
+			
+	text = ''.join(out)
+	out = []
+		
+	in_match = False		
+	for m in re.split('Hub ([0-9]{1,2})', text):
+		if in_match:
+			out.append(f'<a href="hub_texts.html#text_{m}" class="text-link text-white" onmousemove="hubMouseMove(event)" onmouseout="hubMouseOut(event)" data-number="{m}">Hub {m}</a>')
+			in_match = False
+		else:
+			out.append(m)
+			in_match = True
+			
+	return ''.join(out)
+	
+def filter_augment_glyphs(value):
+	global glyphs
+	in_match = False;
+	out = []
+	
+	glyph_table = {g['glyph']:g['number'] for g in glyphs['glyphs']}
+	
+	for c in value:
+		if c not in ' \n\r':
+			out.append(f'<a href="glyphs.html#glyph_{glyph_table[c]}" class="glyph-link text-white ancient-language" onmousemove="glyphMouseMove(event)" onmouseout="glyphMouseOut(event)" data-number="{glyph_table[c]}">{html.escape(c)}</a>')
+		else:
+			out.append(c)
+		
+	return ''.join(out)
 
 jinja_env = Environment(
 	loader=FunctionLoader(load_template),
 	autoescape=select_autoescape(),
 )
 jinja_env.filters['replace_glyph_references'] = filter_replace_glyph_references
+jinja_env.filters['replace_text_references'] = filter_replace_text_references
+jinja_env.filters['augment_glyphs'] = filter_augment_glyphs
 
 def render_template(name, vars):
 	with open(f'dist/{name}','w') as f:
 		template = jinja_env.get_template(name)
 		f.write(template.render(**vars))
 	
-def render_glyphs_page(data):
-	render_template('glyphs.html', {"glyphs": data['glyphs'], 'at': datetime.datetime.fromtimestamp(data['at'])})
+def render_glyphs_page(hubs, walls, glyphs):
+	render_template('glyphs.html', {"hubs": hubs['texts'], "walls": walls['texts'], "glyphs": glyphs['glyphs'], 'at': datetime.datetime.fromtimestamp(glyphs['at'])})
 
-
+def render_hub_texts_page(hubs, walls, glyphs):
+	render_template('hub_texts.html', {"hubs": hubs['texts'], "walls": walls['texts'], "glyphs": glyphs['glyphs'], "texts": hubs["texts"], 'at': datetime.datetime.fromtimestamp(hubs['at'])})
+	
+def render_wall_texts_page(hubs, walls, glyphs):
+	render_template('wall_texts.html', {"hubs": hubs['texts'], "walls": walls['texts'], "glyphs": glyphs['glyphs'], "texts": walls['texts'], 'at': datetime.datetime.fromtimestamp(walls['at'])})
+	
+global glyphs
 if __name__ == '__main__':
 	#main()
-	download_glyphs()
-	render_glyphs_page(load_glyphs())
+	#texts = load_texts()
+	#download_glyphs('glyphs.json')
+	#download_texts('wall_texts.json', WALL_TEXTS_RANGE_NAME, texts['WallTexts'])
+	#download_texts('hub_texts.json', HUB_TEXTS_RANGE_NAME, texts['HubTexts'])
+	glyphs = load_json_file('glyphs.json')
+	hubs = load_json_file('hub_texts.json')
+	walls = load_json_file('wall_texts.json')
+	render_hub_texts_page(hubs, walls, glyphs)
+	render_wall_texts_page(hubs, walls, glyphs)
+	render_glyphs_page(hubs, walls, glyphs)
