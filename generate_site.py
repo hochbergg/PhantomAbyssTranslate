fb3ab32ba877a36881286fbe11b908c5217f9811
Gal Hochberg <hochbergg@gmail.com>
2021-07-05 18:05:30 +0300

    Updated about

--- generate_site.py
+++ generate_site.py
@@ -278,10 +278,10 @@ FILES_TO_COPY = ['AncientLanguage.otf', 'hovers.js', 'darkly.bootstrap.min.css',
 
 global glyphs
 if __name__ == '__main__':
-	texts = load_texts()
-	download_glyphs('tmp/glyphs.json')
-	download_texts('tmp/wall_texts.json', WALL_TEXTS_RANGE_NAME, texts['WallTexts'])
-	download_texts('tmp/hub_texts.json', HUB_TEXTS_RANGE_NAME, texts['HubTexts'])
+	# texts = load_texts()
+	# download_glyphs('tmp/glyphs.json')
+	# download_texts('tmp/wall_texts.json', WALL_TEXTS_RANGE_NAME, texts['WallTexts'])
+	# download_texts('tmp/hub_texts.json', HUB_TEXTS_RANGE_NAME, texts['HubTexts'])
 	glyphs = load_json_file('tmp/glyphs.json')
 	hubs = load_json_file('tmp/hub_texts.json')
 	walls = load_json_file('tmp/wall_texts.json')
